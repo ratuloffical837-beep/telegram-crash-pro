05383d894445fbe8b56c98e782e0ef06277b13de
ratuloffical837-beep <ratuloffical837@gmail.com>
2026-01-03 13:25:56 +0600

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
